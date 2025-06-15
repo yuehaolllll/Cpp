b6242004fc1ab7952e026c82bb63892bc51cf11e
yuehaolllll <3482243384@qq.com>
2025-06-15 16:33:35 +0800

    first

---
+++ day01/01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout <<"hello C++"<< endl;
+    int a = 10;
+    cout << "a = " << a << endl;
+    cout << "a = " << a << endl;
+    system("pause");
+
+    return 0;
+
+}
+
