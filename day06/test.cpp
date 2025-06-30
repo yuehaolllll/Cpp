bc2edf2d94ac671dcaa36dad6441e1a5d3438d50
yuehaolllll <3482243384@qq.com>
2025-06-30 19:31:40 +0800

    update

--- day06/test.cpp
+++ day06/test.cpp
@@ -13,8 +13,18 @@ public:
     // 函数名与类名相同
     // 可以有参数，可以发生重载
     // 创建对象时会被自动调用，而且只调用一次
+
+    /* 构造函数的分类： 普通构造函数与拷贝构造函数 */
     Person(){
-        cout << "这是构造函数" << endl;
+        cout << "这是无参构造函数" << endl;
+    }
+    Person(int a){
+        age = a;
+        cout << "这是有参构造函数" << endl;
+    }
+    Person(const Person &p){
+        age = p.age;
+        cout << "这是拷贝构造函数" << endl;
     }
 
     /* 析构函数 */
@@ -26,20 +36,34 @@ public:
         cout << "这是析构函数" << endl;
     }
 
+    int age;
+
 };
 
 void test1(){
 
-    Person p1;
+    // 1、括号法
+    // Person p1;          /* 默认构造函数调用 */
+    // Person p2(10);      /* 有参构造函数调用 */
+    // Person p3(p2);      /* 拷贝构造函数调用 */
 
+    // cout << "p2的年龄-> " << p2.age << endl;
+    // cout << "p3的年龄-> " << p3.age << endl;
+    
+
+    // 2、显示法
+    Person p1;
+    Person p2 = Person(10);
+    Person p3 = Person(p2);
 }
 
 int main(){
 
-    // test1();
-    Person p2;
+    test1();
+    
 
     system("pause");
     return 0;
 
 }
+
